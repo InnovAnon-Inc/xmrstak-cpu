673b3678fb40b3cac1b538a804da9d5e272c0f44
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2019-03-05 21:47:54 -0600

    trying to open git server ports

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 set -exo nounset
 
-[ $# -eq 1 ] || exit 1
+[ $# -eq 2 ] || exit 1
 T="`sed 's#/$##' <<< "$1"`"
+P=$2
 
 trap "sudo docker rmi -f $T" 0
 
 ./systemctl.sh $T
-sudo docker run -ti $T /bin/sh
+sudo docker run -p $P -ti $T /bin/sh
