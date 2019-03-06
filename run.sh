c1cc21bd5f973ea696c1c4214a2923dcdbfda3f1
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2019-03-06 10:56:43 -0600

    added test container

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 set -exo nounset
 
-[ $# -ge 1 ] || exit 1
-[ -f $1/port -o $# -eq 2 ] || exit 1
+[ $# -eq 1 ] || exit 1
+#[ $# -ge 1 ] || exit 1
+#[ -f $1/port -o $# -eq 2 ] || exit 1
 T="`sed 's#/$##' <<< "$1"`"
-if [ -f $1/port ] ; then
-	P="`cat $1/port`"
-else
-	P=$2
-fi
+#if [ -f $1/port ] ; then
+#	P="`cat $1/port`"
+#else
+#	P=$2
+#fi
 
 trap "sudo docker rmi -f $T" 0
 
 ./systemctl.sh $T
-sudo docker run -p $P -ti $T /bin/sh
+#sudo docker run -p $P -ti $T /bin/sh
+#sudo docker run --publish-all=true -ti $T /bin/sh
+sudo docker run -Pti $T /bin/sh
---
+++ test/start.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+set -exo nounset
+
+netcat -l 1000
