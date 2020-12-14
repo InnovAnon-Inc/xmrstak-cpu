429069ca0287b6f5966ea8c26af0070f8c0027c3
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-12-14 04:30:03 -0600

    update

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 (( ! $# ))
 (( $UID ))
 for DOCKER_TAG in nehalem nocona sandybridge core2 ppc7450 ; do
-  DOCKER_TAG="$DOCKER_TAG"         \
-  docker-compose build \
+  DOCKER_TAG="$DOCKER_TAG"               \
+  docker-compose build                   \
+    --build-arg DOCKER_TAG="$DOCKER_TAG" \
     --build-arg CFLAGS="-g0 -Ofast -ffast-math -fassociative-math -freciprocal-math -fmerge-all-constants -fipa-pta -floop-nest-optimize -fgraphite-identity -floop-parallelize-all"
 done
 
